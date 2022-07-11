c32a93ad77bdda7ad56425e2f05143909639f8b9
kamron <sadikovkamron@gmail.com>
2022-07-11 17:11:30 +0500

    project

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt('Введите своё имя');
+console.log("Ваше имя " + name);
+
+var age = +prompt('Введите свой возраст')
+console.log("Ваш возраст " + age);
+
+var addition = +prompt('Введите правельный ответ (32 + 14 = ?');
+console.log("Ваш ответ " + addition + ' Правельный ответ ' + (32+14));
+
+var subtraction = +prompt('Введите правельный ответ (64 - 24 = ?');
+console.log("Ваш ответ " + subtraction + ' Правельный ответ ' + (64-24));
+
+var multiplication = +prompt('Введите правельный ответ (8 * 7 = ?');
+console.log("Ваш ответ " + multiplication + ' Правельный ответ ' + (8*7));
+
+var division = +prompt('Введите правельный ответ (56 / 7 = ?');
+console.log("Ваш ответ " + division + ' Правельный ответ ' + (56/7));
+
+var percent = +prompt('Введите отстаток (10 % 3 = ?');
+console.log("Ваш ответ " + percent + ' Правельный ответ ' + (10%3));
+
+alert('Откройте console для проверки результата');
